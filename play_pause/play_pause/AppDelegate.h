4c25b0df80b336e10afeaaddb704c80b82603262
Serhii Krapivko <serhii.krapivko@echostar.com>
2016-05-13 15:40:29 +0300

    added empty project

---
+++ play_pause/play_pause/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  play_pause
+//
+//  Created by Serhii Krapivko on 5/13/16.
+//  Copyright © 2016 Serhii Krapivko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ play_pause/play_pause/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  play_pause
+//
+//  Created by Serhii Krapivko on 5/13/16.
+//  Copyright © 2016 Serhii Krapivko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
